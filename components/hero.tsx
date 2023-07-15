12e9c0ee5c1ba21ca09454341c984ebc01e3bd4c
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-15 08:51:08 -0500

    Heroe edited

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,12 +1,21 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
+'use client';
+import VideoThumb from '@/public/images/hero-image.png';
+import ModalVideo from '@/components/modal-video';
+import { Button } from '@material-tailwind/react';
+import { MouseEvent } from 'react';
 
 export default function Hero() {
+  const handleMouseDown = (event: MouseEvent) => {
+    event.currentTarget.classList.add('scale-90');
+  };
+
+  const handleMouseUp = (event: MouseEvent) => {
+    event.currentTarget.classList.remove('scale-90');
+  };
   return (
     <section className="relative">
-
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
+      {/* <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
         <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
@@ -20,42 +29,77 @@ export default function Hero() {
             <circle cx="155" cy="443" r="64" />
           </g>
         </svg>
-      </div>
+      </div> */}
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
+      <div className="flex items-center max-w-6xl mx-auto px-4 sm:px-6  min-h-screen">
         {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
+        <div>
           {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
+          <div className="text-center">
+            <h1
+              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              Crea una{' '}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                camisa
+              </span>{' '}
+              tan{' '}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                unica
+              </span>{' '}
+              como{' '}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                tu
+              </span>
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+              <p
+                className="text-xl text-gray-600 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                Crea camisas a tu gusto con ayuda de una IA.
+              </p>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none flex justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+                  <Button
+                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 shadow-blue-200"
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}
+                  >
+                    Iniciar creacion
+                  </Button>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                  <Button
+                    className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:ml-4 shadow-gray-300"
+                    // href="#0"
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}
+                  >
+                    Imprimir mi diseño
+                  </Button>
                 </div>
               </div>
             </div>
           </div>
 
           {/* Hero image */}
-          <ModalVideo
+          {/* <ModalVideo
             thumb={VideoThumb}
             thumbWidth={768}
             thumbHeight={432}
             thumbAlt="Modal video thumbnail"
             video="/videos/video.mp4"
             videoWidth={1920}
-            videoHeight={1080} />
-
+            videoHeight={1080} /> */}
         </div>
-
       </div>
     </section>
-  )
+  );
 }
