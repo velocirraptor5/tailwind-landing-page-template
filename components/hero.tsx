cd79736a12774e791acd783b34639e71cd66af9d
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-21 18:06:07 -0500

    cube component, small changes and heroe small clean

--- components/hero.tsx
+++ components/hero.tsx
@@ -14,23 +14,6 @@ export default function Hero() {
   };
   return (
     <section className="relative">
-      {/* Illustration behind hero content */}
-      {/* <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-          <g fill="url(#illustration-01)" fillRule="evenodd">
-            <circle cx="1232" cy="128" r="128" />
-            <circle cx="155" cy="443" r="64" />
-          </g>
-        </svg>
-      </div> */}
-
       <div className="flex items-center max-w-6xl mx-auto px-4 sm:px-6  min-h-screen">
         {/* Hero content */}
         <div>
@@ -40,15 +23,15 @@ export default function Hero() {
               className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
               data-aos="zoom-y-out"
             >
-              Crea una{' '}
+              {'Crea una '}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 camisa
-              </span>{' '}
-              tan{' '}
+              </span>
+              {' tan '}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 unica
-              </span>{' '}
-              como{' '}
+              </span>
+              {' como '}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 tu
               </span>
@@ -78,7 +61,6 @@ export default function Hero() {
                 <div>
                   <Button
                     className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:ml-4 shadow-gray-300"
-                    // href="#0"
                     onMouseDown={handleMouseDown}
                     onMouseUp={handleMouseUp}
                   >
@@ -88,16 +70,6 @@ export default function Hero() {
               </div>
             </div>
           </div>
-
-          {/* Hero image */}
-          {/* <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} /> */}
         </div>
       </div>
     </section>
--- components/utils/cube.tsx
+++ components/utils/cube.tsx
@@ -1,23 +1,30 @@
+import { HtmlProps } from 'next/dist/shared/lib/html-context';
 import { FaCube } from 'react-icons/fa';
 
-interface CubeProps {
+interface CubeProps extends React.HTMLAttributes<HTMLAnchorElement> {
   isDark: boolean;
 }
 
-export default function Cube({ isDark }: CubeProps) {
+export default function Cube({ isDark, ...props }: CubeProps) {
   return (
     <div className="flex justify-center items-center ml-3 ">
       <a
         onClick={(e) => {
           e.preventDefault();
         }}
-        href=""
+        {...props}
       >
         <FaCube
           className={`h4 transition duration-500 text-gray-500 hover:${
             isDark ? 'text-white' : 'text-black'
           }`}
         />
+      </a>
+      {/* ghost elementes */}
+      {/* es importante esta parte debido a que hay momentos en que el css no es carga adecuadamente en los condicionales de antes y esto fuerza a cargar todas las variantes */}
+      <a href="" hidden={true}>
+        <FaCube className={'text-gray-500 hover:text-white'} />
+        <FaCube className={'text-gray-500 hover:text-black'} />
       </a>
     </div>
   );
