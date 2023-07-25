8846602a6f7252b27d2164e8be2def197f60d576
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-24 20:42:08 -0500

    logo inserted and brand logos implemented

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,22 +3,34 @@ import VideoThumb from '@/public/images/hero-image.png';
 import ModalVideo from '@/components/modal-video';
 import { Button } from '@material-tailwind/react';
 import { MouseEvent } from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { FaChevronDown } from 'react-icons/fa';
 
 export default function Hero() {
-  const handleMouseDown = (event: MouseEvent) => {
-    event.currentTarget.classList.add('scale-90');
+  // const handleMouseDown = (event: MouseEvent) => {
+  //   event.currentTarget.classList.add('scale-90');
+  // };
+
+  // const handleMouseUp = (event: MouseEvent) => {
+  //   event.currentTarget.classList.remove('scale-90');
+  // };
+
+  const handleScrollDownClick = () => {
+    window.scrollTo({ top: window.innerHeight, behavior: 'auto' });
   };
 
-  const handleMouseUp = (event: MouseEvent) => {
-    event.currentTarget.classList.remove('scale-90');
-  };
   return (
     <section className="relative">
-      <div className="flex items-center max-w-6xl mx-auto px-4 sm:px-6  min-h-screen">
+      {/* Illustration behind hero content */}
+      <div className="absolute w-screen h-screen" data-aos="zoom-y-out">
+        <Image src={'/images/WolfId (1).svg'} fill={true} alt="logo" />
+      </div>
+      <div className="flex items-center justify-center max-w-6xl mx-auto px-4 sm:px-6  min-h-screen">
         {/* Hero content */}
         <div>
           {/* Section header */}
-          <div className="text-center">
+          {/* <div className="text-center">
             <h1
               className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
               data-aos="zoom-y-out"
@@ -69,9 +81,23 @@ export default function Hero() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
+      <motion.button
+        className="scroll-down-icon w-12 h-12 animate-bounce text-wolfTeal-900"
+        animate={{ y: [0, 10] }}
+        transition={{ repeat: Infinity, duration: 1 }}
+        style={{
+          position: 'absolute',
+          bottom: '20px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+        onClick={handleScrollDownClick}
+      >
+        <FaChevronDown />
+      </motion.button>
     </section>
   );
 }
