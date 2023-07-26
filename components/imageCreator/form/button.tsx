8f6d3b540cdfe691d44bc1b836047bc956d3f5d8
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-25 21:56:19 -0500

    button in form small refactor

--- components/imageCreator/form/button.tsx
+++ components/imageCreator/form/button.tsx
@@ -1,9 +1,16 @@
 import { DarkBgContext } from '@/contexts/DarkBgContext';
 import { Button } from '@material-tailwind/react';
-import { useContext } from 'react';
+import { MouseEvent, useContext } from 'react';
 
 export default function FormButton() {
   const { isDark } = useContext(DarkBgContext);
+  const handleMouseDown = (event: MouseEvent) => {
+    event.currentTarget.classList.add('scale-90');
+  };
+
+  const handleMouseUp = (event: MouseEvent) => {
+    event.currentTarget.classList.remove('scale-90');
+  };
   return (
     <div>
       <Button
@@ -12,15 +19,9 @@ export default function FormButton() {
             ? 'text-black bg-white  hover:bg-slate-200 '
             : 'text-white bg-wolfBg-900  hover:bg-wolfBg-700 '
         }`}
-        onMouseDown={(e) => {
-          e.currentTarget.classList.add('scale-90');
-        }}
-        onMouseUp={(e) => {
-          e.currentTarget.classList.remove('scale-90');
-        }}
-        onMouseOut={(e) => {
-          e.currentTarget.classList.remove('scale-90');
-        }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseOut={handleMouseUp}
       >
         <p className="text-auto">crear</p>
       </Button>
