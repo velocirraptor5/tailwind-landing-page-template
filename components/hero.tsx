8d8ef40161e6ba382b6b12bb14f5da5e58960d0b
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-25 21:54:13 -0500

    hero comments removed and logo to jsx for future works

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,16 +6,9 @@ import { MouseEvent } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { FaChevronDown } from 'react-icons/fa';
+import Logo from '@/public/images/Logo';
 
 export default function Hero() {
-  // const handleMouseDown = (event: MouseEvent) => {
-  //   event.currentTarget.classList.add('scale-90');
-  // };
-
-  // const handleMouseUp = (event: MouseEvent) => {
-  //   event.currentTarget.classList.remove('scale-90');
-  // };
-
   const handleScrollDownClick = () => {
     window.scrollTo({ top: window.innerHeight, behavior: 'auto' });
   };
@@ -30,58 +23,10 @@ export default function Hero() {
         {/* Hero content */}
         <div>
           {/* Section header */}
-          {/* <div className="text-center">
-            <h1
-              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
-              data-aos="zoom-y-out"
-            >
-              {'Crea una '}
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
-                camisa
-              </span>
-              {' tan '}
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
-                unica
-              </span>
-              {' como '}
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+          {/* <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 tu
               </span>
-            </h1>
-            <div className="max-w-3xl mx-auto">
-              <p
-                className="text-xl text-gray-600 mb-8"
-                data-aos="zoom-y-out"
-                data-aos-delay="150"
-              >
-                Crea camisas a tu gusto con ayuda de una IA.
-              </p>
-              <div
-                className="max-w-xs mx-auto sm:max-w-none flex justify-center"
-                data-aos="zoom-y-out"
-                data-aos-delay="300"
-              >
-                <div>
-                  <Button
-                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 shadow-blue-200"
-                    onMouseDown={handleMouseDown}
-                    onMouseUp={handleMouseUp}
-                  >
-                    Iniciar creacion
-                  </Button>
-                </div>
-                <div>
-                  <Button
-                    className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:ml-4 shadow-gray-300"
-                    onMouseDown={handleMouseDown}
-                    onMouseUp={handleMouseUp}
-                  >
-                    Imprimir mi diseño
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div> */}
+             */}
         </div>
       </div>
       <motion.button
