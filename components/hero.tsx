35999d9bc37ea2ae66fa0a3c2ebb6a87d5918257
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-28 18:06:43 -0500

    hero modified, whit logo and it animation

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,32 +2,116 @@
 import VideoThumb from '@/public/images/hero-image.png';
 import ModalVideo from '@/components/modal-video';
 import { Button } from '@material-tailwind/react';
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
 import { FaChevronDown } from 'react-icons/fa';
 import Logo from '@/public/images/Logo';
 
 export default function Hero() {
+  const [visible, setVisible] = useState(false);
+  const [logoVisible, setLogoVisible] = useState(false);
+  const [slogan, setSlogan] = useState(true);
+
   const handleScrollDownClick = () => {
-    window.scrollTo({ top: window.innerHeight, behavior: 'auto' });
+    window.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
+  };
+  const textVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      transition: {
+        duration: 1,
+      },
+      y: 100,
+      marginTop: '100%',
+    },
+    visible: {
+      opacity: 1,
+      transition: { duration: 8, delay: 2 },
+      y: 0,
+      marginTop: 0,
+    },
   };
 
   return (
     <section className="relative">
       {/* Illustration behind hero content */}
       <div className="absolute w-screen h-screen" data-aos="zoom-y-out">
-        <Image src={'/images/WolfId (1).svg'} fill={true} alt="logo" />
+        {/* <Image src={'/images/WolfId (1).svg'} fill={true} alt="logo" /> */}
+        <Logo visible={logoVisible} />
       </div>
-      <div className="flex items-center justify-center max-w-6xl mx-auto px-4 sm:px-6  min-h-screen">
+      <div className="flex flex-col text-center justify-center items-center w-screen h-screen">
         {/* Hero content */}
-        <div>
-          {/* Section header */}
-          {/* <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
-                tu
-              </span>
-             */}
-        </div>
+        <motion.div className="flex-col z-10 transition duration-500">
+          <AnimatePresence>
+            <motion.div
+              variants={textVariants}
+              initial="hidden"
+              animate={visible ? 'hidden' : 'visible'}
+              className="font-madsense  xl:text-9xl lg:text-8xl md:text-7xl text-6xl bg-clip-text text-transparent bg-gradient-to-r from-wolfTeal-900 to-white"
+              onAnimationComplete={() => {
+                if (!visible) {
+                  setSlogan(false);
+                  setTimeout(() => {
+                    setLogoVisible(!visible);
+                    setTimeout(() => {
+                      setVisible(!visible);
+                      setSlogan(true);
+                    }, 7000);
+                  }, 10000);
+                } else {
+                  // setSlogan(true);
+                  setVisible(!visible);
+                  setLogoVisible(!visible);
+                }
+              }}
+            >
+              WOLF DI
+            </motion.div>
+
+            <motion.div
+              key={'slogan'}
+              variants={textVariants}
+              initial="hidden"
+              animate={visible ? 'hidden' : 'visible'}
+              exit="hidden"
+              className="font-varino lg:text-2xl md:text-xl text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-wolfTeal-900 to-white"
+            >
+              El futuro es nuestro lienzo
+            </motion.div>
+
+            {!slogan && (
+              <motion.div
+                key={'Continue'}
+                initial={{
+                  opacity: 0,
+                  height: 0,
+                  marginTop: 0,
+                  marginBottom: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  height: 'auto',
+                  transition: { duration: 1 },
+                  marginTop: '30%',
+                  marginBottom: '30%',
+                  y: 100,
+                }}
+                exit={{
+                  opacity: 0,
+                  height: 0,
+                  transition: { duration: 1 },
+                  marginTop: 0,
+                  y: 0,
+                }}
+                className="font-varino text-auto font-bold bg-clip-text cursor-pointer text-transparent bg-gradient-to-r from-wolfTeal-900 to-white"
+                onClick={handleScrollDownClick}
+              >
+                Desliza y crea una camisa tan unica como tu
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
       </div>
       <motion.button
         className="scroll-down-icon w-12 h-12 animate-bounce text-wolfTeal-900"
@@ -35,7 +119,7 @@ export default function Hero() {
         transition={{ repeat: Infinity, duration: 1 }}
         style={{
           position: 'absolute',
-          bottom: '20px',
+          bottom: '0',
           left: '50%',
           transform: 'translateX(-50%)',
         }}
