894949231117fd1b4c74208f4c46a1530087c21d
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-28 18:10:50 -0500

    title font change

--- components/utils/title.tsx
+++ components/utils/title.tsx
@@ -5,8 +5,8 @@ interface titleProps {
 export default function Title({ isDark = false, text }: titleProps) {
   return (
     <div
-      className={`font-bold title-auto transition duration-500 ${
-        isDark ? 'text-white' : ''
+      className={`icon-auto font-varino font-bold transition duration-500 ${
+        isDark ? 'text-wolfTeal-900' : ''
       }`}
     >
       {text}
