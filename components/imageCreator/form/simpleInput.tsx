f8bf818855f889095cee27ddece0a2b483dd2127
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-28 18:10:22 -0500

    simple imput text hover interaction changed

--- components/imageCreator/form/simpleInput.tsx
+++ components/imageCreator/form/simpleInput.tsx
@@ -1,7 +1,7 @@
 import Cube from '@/components/utils/cube';
 import Title from '@/components/utils/title';
 import { DarkBgContext } from '@/contexts/DarkBgContext';
-import { Dispatch, SetStateAction, useContext } from 'react';
+import { Dispatch, SetStateAction, useContext, useState } from 'react';
 
 interface InputProps {
   title: string;
@@ -14,6 +14,7 @@ export default function SimpleInput({
   setAtribute,
 }: InputProps) {
   const { isDark } = useContext(DarkBgContext);
+  const [focus, setFocus] = useState(false);
   return (
     <div>
       {/* Title */}
@@ -24,14 +25,17 @@ export default function SimpleInput({
         <div>
           <input
             type="text"
-            className={`shadow-md border-2 rounded-lg bg-transparent w-full transition duration-500
-                        ${isDark ? 'text-white text-auto' : 'text-auto'}
-                        hover:${isDark ? 'border-white' : 'border-black'}
-                        focus:ring-transparent
-                        focus:${isDark ? 'border-white' : 'border-black'}
-                        `}
+            className={`shadow-md border-2 rounded-lg bg-transparent w-full transition duration-500 text-auto  hover:${
+              isDark ? 'border-white' : 'border-black'
+            } hover:${isDark ? 'text-white' : 'text-black'} ${
+              focus ? (isDark ? 'text-white' : 'text-black') : 'text-gray-600'
+            } focus:ring-transparent focus:${
+              isDark ? 'border-white' : 'border-black'
+            }`}
             value={atribute}
             onChange={(e) => setAtribute(e.target.value)}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
           />
         </div>
         <Cube isDark={isDark} />
