37da175e604a7e5c11814ef7788ba45e4a7245cf
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-28 18:13:06 -0500

    image animation fixed

--- components/imageCreator/images.tsx
+++ components/imageCreator/images.tsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Images() {
   const images = [
@@ -10,61 +10,92 @@ export default function Images() {
     { id: 4, src: '/images/_f5f87b4f-ea75-4e0f-b3c7-1af58dafabc9.jpg' },
   ];
 
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  const handleResize = () => setScreenWidth(window.innerWidth);
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  const [mainImageSize, setMainImageSize] = useState(600);
+  const [imagesSize, setImagesSize] = useState(150);
+
+  useEffect(() => {
+    if (screenWidth > 1280) {
+      setImagesSize(150);
+      setMainImageSize(600);
+      return;
+    }
+    if (screenWidth > 1024) {
+      setImagesSize(112);
+      setMainImageSize(471);
+      return;
+    }
+    if (screenWidth > 768) {
+      setImagesSize(84);
+      setMainImageSize(353);
+      return;
+    }
+    setImagesSize(70);
+    setMainImageSize(295);
+    return;
+  }, [screenWidth]);
+
   const [selectedImage, setSelectedImage] = useState(images[0]);
 
   return (
     <div className="col-span-6 flex justify-center my-10" data-aos="fade-left">
-      <div className="flex flex-col justify-center ">
-        <AnimatePresence>
+      <div className="flex flex-col justify-center overflow-hidden">
+        <div
+          className="overflow-hidden mb-5 aspect-square transition duration-500"
+          style={{ width: mainImageSize }}
+        >
           {images.map((image) => (
-            <div key={image.id}>
+            <AnimatePresence key={image.id}>
               {image.id === selectedImage.id && (
-                <motion.div
-                  initial={{ opacity: 0, x: 300 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: -300 }}
-                >
-                  <Image
-                    width={600}
-                    height={600}
-                    src={selectedImage.src}
-                    alt="selected"
-                  />
-                </motion.div>
+                <motion.img
+                  key={image.id}
+                  initial={{
+                    opacity: 0,
+                    width: 0,
+                    height: 0,
+                    x: -100,
+                    y: -100,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    width: mainImageSize,
+                    height: mainImageSize,
+                    x: 0,
+                    y: 0,
+                  }}
+                  exit={{ opacity: 0, width: 0, height: 0, x: 100, y: 100 }}
+                  transition={{ duration: 1 }}
+                  src={selectedImage.src}
+                  alt="selecciona para recargar la imagen"
+                />
               )}
-            </div>
+            </AnimatePresence>
           ))}
-        </AnimatePresence>
-        <div>
+        </div>
+        <div className="flex align-middle justify-center">
           {images.map((image) => (
-            <button
-              key={image.id}
-              onClick={() => setSelectedImage(image)}
-              style={{
-                border: 'none',
-                margin: '10px',
-                padding: '0',
-                cursor: 'pointer',
-              }}
-            >
-              <AnimatePresence>
-                {selectedImage.id !== image.id && (
-                  <motion.div
-                    key={image.id}
-                    initial={{ opacity: 0, width: 0 }}
-                    animate={{ opacity: 1, width: 'auto' }}
-                    exit={{ opacity: 0, width: 0 }}
-                  >
-                    <Image
-                      src={image.src}
-                      width={150}
-                      height={150}
-                      alt="preview"
-                    />
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </button>
+            <AnimatePresence key={image.id}>
+              {selectedImage.id !== image.id && (
+                <motion.img
+                  key={image.id}
+                  initial={{ opacity: 0, width: 0, marginRight: 0 }}
+                  animate={{ opacity: 1, width: imagesSize, marginRight: 5 }}
+                  exit={{ opacity: 0, width: 0, marginRight: 0 }}
+                  transition={{ duration: 1 }}
+                  onClick={() => setSelectedImage(image)}
+                  src={image.src}
+                  alt="selecciona para recargar la imagen"
+                />
+              )}
+            </AnimatePresence>
+            // </button>
           ))}
         </div>
       </div>
