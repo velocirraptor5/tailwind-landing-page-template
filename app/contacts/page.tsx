1cde5662866ff23569f572d4c935ef3338b30a37
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2023-07-31 18:03:34 -0500

    contacts page created

---
+++ app/contacts/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Contact page - Simple',
+  description: 'Page description',
+};
+
+export default function contacts() {
+  return <div className=" bg-red-300"> a</div>;
+}
